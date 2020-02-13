67cb33efad716b55bfbe984d983fa8954cea6d57
sarkou <58863062+sarkou@users.noreply.github.com>
2020-02-14 00:45:06 +0100

    Update Couloir.java

--- domotique/src/domotique/Couloir.java
+++ domotique/src/domotique/Couloir.java
@@ -1,5 +1,14 @@
 package domotique;
 
-public class Couloir {
 
+// cette classe permet d'allumer la lumière
+
+public class AllumerLumiere implements Commande{
+	private Lumiere lumineux;
+	public AllumerLumiere(Lumiere lumineux) {
+		this.lumineux=lumineux;
+	}
+	public void execute() {
+		lumineux.jallume();
+	}
 }
